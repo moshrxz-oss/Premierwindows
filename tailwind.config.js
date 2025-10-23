48663ba351c4fad7cb1fd651b965d3855cfd4e29
Mohammed Shareef <moshrxz@gmail.com>
2025-10-23 18:34:08 -0400

    new commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primaryBlue: "#002b5c",
+        accentGreen: "#00b388",
+        softTeal: "#89efd6",
+        darkGray: "#303135",
+        highlightGreen: "#00d958",
+      },
+    },
+  },
+  plugins: [],
+}
